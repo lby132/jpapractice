8b7f0122069affcb721048434dd19f070468a81a
lby132 <quddbscjswow@naver.com>
2022-11-27 13:40:14 +0900

    jpa 코드 반복학습중(안돌아감)

--- src/main/java/ev/jpapractice/domain/item/Order.java
+++ src/main/java/ev/jpapractice/domain/item/Order.java
@@ -41,8 +41,49 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-//    public void setMember(Member member) {
-//        this.member = member;
-//        member.getO
-//    }
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void addDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        final Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 }
--- src/main/java/ev/jpapractice/service/OrderService.java
+++ src/main/java/ev/jpapractice/service/OrderService.java
@@ -8,9 +8,12 @@ import ev.jpapractice.domain.item.Order;
 import ev.jpapractice.repository.ItemRepository;
 import ev.jpapractice.repository.MemberRepository;
 import ev.jpapractice.repository.OrderRepository;
+import ev.jpapractice.repository.OrderSearch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -21,18 +24,33 @@ public class OrderService {
     private final MemberRepository memberRepository;
     private final ItemRepository itemRepository;
 
-//    @Transactional
-//    public Long order(Long memberId, Long itemId, int count) {
-//
-//        final Member member = memberRepository.findOne(memberId);
-//        final Item item = itemRepository.findOne(itemId);
-//
-//        final Delivery delivery = new Delivery();
-//        delivery.setAddress(member.getAddress());
-//
-//        final OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
-//
-//        Order.
-//
-//    }
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        final Member member = memberRepository.findOne(memberId);
+        final Item item = itemRepository.findOne(itemId);
+
+        final Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+
+        final OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        final Order order = Order.createOrder(member, delivery, orderItem);
+
+        orderRepository.save(order);
+
+        return order.getId();
+
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId) {
+        final Order order = orderRepository.findOne(orderId);
+
+        order.cancel();
+    }
+
+    public List<Order> findOrders(OrderSearch orderSearch) {
+        return orderRepository.findAllByString(orderSearch);
+    }
 }
